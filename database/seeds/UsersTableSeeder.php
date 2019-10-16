2f141f7a5a2ba26195d27fb423b0b330a007dc72
Ryan Shirley <info@ryanshirley.ie>
2019-10-16 11:49:08 +0100

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,19 +18,11 @@ class UsersTableSeeder extends Seeder
 
         $admin = new User();
         $admin->name = 'Ryan';
-        $admin->email = 'ryanjshirley@gmail.com';
+        $admin->email = 'ryan@example.com';
         $admin->password = bcrypt('secret');
         $admin->approved_at = now();
         $admin->save();
         $admin->roles()->attach($role_admin);
-
-        $admin2 = new User();
-        $admin2->name = 'Emily Darlington';
-        $admin2->email = 'emilyjane.darlington@gmail.com';
-        $admin2->password = bcrypt('secret');
-        $admin2->approved_at = now();
-        $admin2->save();
-        $admin2->roles()->attach($role_admin);
 
         $leader = new User();
         $leader->name = 'Leader Name';
