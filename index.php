f63f032ccc7847dcb848ca6bb74a70d3f1062f07
Ryan Shirley <N00161198@student.iadt.ie>
2021-08-14 20:05:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../../../../laravel/college/professional-practice/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/../../../../laravel/college/professional-practice/vendor/autol
 |
 */
 
-$app = require_once __DIR__.'/../../../../laravel/college/professional-practice/bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
