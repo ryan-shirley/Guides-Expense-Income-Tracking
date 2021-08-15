7d42c96ecb35869f71db1953497b60310b686310
Ryan Shirley <ryanjshirley@gmail.com>
2021-08-15 13:01:03 +0100

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(RolesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(PaymentsTableSeeder::class);
-        $this->call(IncomesTableSeeder::class);
-        $this->call(BankAccountsTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(PaymentsTableSeeder::class);
+        // $this->call(IncomesTableSeeder::class);
+        // $this->call(BankAccountsTableSeeder::class);
+        $this->call(FixUsersSeeder::class);
+        $this->call(FixIncomesSeeder::class);
+        $this->call(FixEventsSeeder::class);
+        $this->call(FixBankAccountSeeder::class);
     }
 }
