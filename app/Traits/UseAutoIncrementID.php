97914ff2ca9f1528d56bfa1464c89688c189573e
Ryan Shirley <N00161198@student.iadt.ie>
2021-11-07 16:33:45 +0000

    Fix object ids not being created

--- app/Traits/UseAutoIncrementID.php
+++ app/Traits/UseAutoIncrementID.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace App\Traits;
 
 
 trait UseAutoIncrementID {
-
     /**
      * Increment the counter and get the next sequence
      * 
@@ -51,24 +51,24 @@ trait UseAutoIncrementID {
         }
     }
 
-    /**
-     * Boot the AutoIncrementID trait for the model.
-     *
-     * @return void
-     */
-    public static function bootUseAutoIncrementID() {
-        static::creating(function ($model) {
-            $model->incrementing = false;
-            $model->{$model->getKeyName()} = self::getID($model->getTable());
-        });
-    }
+//     /**
+//      * Boot the AutoIncrementID trait for the model.
+//      *
+//      * @return void
+//      */
+//     public static function bootUseAutoIncrementID() {
+//         static::creating(function ($model) {
+//             $model->incrementing = false;
+//             $model->{$model->getKeyName()} = self::getID($model->getTable());
+//         });
+//     }
 
-    /**
-     * Get the casts array.
-     *
-     * @return array
-     */
-    public function getCasts() {
-        return $this->casts;
-    }
+//     /**
+//      * Get the casts array.
+//      *
+//      * @return array
+//      */
+//     public function getCasts() {
+//         return $this->casts;
+//     }
 }
