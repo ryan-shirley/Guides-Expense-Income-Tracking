014e6862bcf13581a06e576e164cbb4318ad0dde
Ryan Shirley <ryanjshirley@gmail.com>
2023-04-08 18:46:31 +0100

    Force images to be jpg

--- app/Traits/ImageHandler.php
+++ app/Traits/ImageHandler.php
@@ -13,9 +13,25 @@ trait ImageHandler
         $imageFolder = 'payments';
 
         // Image file name
-        $filename = 'p_' . $paymentRefId . '_' . $paymentId . '.' . $imageToSave->extension();
+        $filename = 'p_' . $paymentRefId . '_' . $paymentId . '.jpg';
+        $interventionImage = \ImageConvert::make($imageToSave)->stream("jpg", 80);
 
         // Save image on server
-        $savedImagePath = $imageToSave->storeAs($imageFolder, $filename);
+        $savedImagePath = $interventionImage->storeAs($imageFolder, $filename);
     }
+
+    public function GetReceiptUrl($paymentRefId, $paymentId) {
+        $domainPath = env('GOOGLE_CLOUD_STORAGE_API_PATH');
+
+        if($domainPath == null) {
+            return '';
+        }
+
+        // Image Folder
+        $imageFolder = 'payments';
+
+        // Image file url
+        return $domainPath . '/' . $imageFolder . 'p_' . $paymentRefId . '_' . $paymentId . '.jpg';
+    }
+
 }
